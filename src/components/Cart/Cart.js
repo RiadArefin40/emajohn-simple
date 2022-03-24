2e4b8ebb79f92be98c0322f4bca7aed15b1ce917
RiadArefin <arefinriad40@gmail.com>
2022-03-24 14:57:37 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = (props) => {
+    return (
+        <div>
+            <p>hi</p>
+            <p>item:{props.cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
